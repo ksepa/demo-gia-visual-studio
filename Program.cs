a30f8e9c2da6a1f6620006d2f71c0c2adba8cb18
Ksepa <stavros_x@hotmail.com>
2019-06-26 20:30:41 +0300

    prwth ekdosh

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_gia_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kalhmera");
+        }
+    }
+}
